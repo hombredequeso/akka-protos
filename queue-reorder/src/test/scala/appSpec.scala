faed1bf4b0da3c3f5465103b4433146807cb5fe3
markc <markc@pageuppeople.com>
2018-03-30 13:45:39 +1100

    Add test for correct draining of queue

--- queue-reorder/src/test/scala/appSpec.scala
+++ queue-reorder/src/test/scala/appSpec.scala
@@ -7,10 +7,18 @@ import scala.concurrent.duration._
 import SourceQueue._
 
 object TestStream {
+
   case class StreamStart(n: Int)
   implicit def toInt(s: StreamStart) = s.n
   implicit val defaultStreamStart = StreamStart(0)
-  def orderedMessageStream(implicit start: StreamStart) = Stream.from(start).map(i => Message(i,i))
+
+  case class StreamEnd(n: Int)
+  implicit def toInt(s: StreamEnd) = s.n
+  implicit val defaultStreamEnd = StreamEnd(12)
+
+  def infiniteOrderedMessageStream(implicit start: StreamStart) = Stream.from(start).map(i => Message(i,i))
+  def finiteOrderedMessageStream(implicit start: StreamStart, end: StreamEnd) = 
+        Stream.range(start.n, end.n, 1).map(i => Message(i,i))
 }
 
 class QueueReorderSpec(_system: ActorSystem)
@@ -19,7 +27,7 @@ class QueueReorderSpec(_system: ActorSystem)
   with FlatSpecLike
   with BeforeAndAfterAll {
 
-  import TestStream.orderedMessageStream
+  import TestStream.infiniteOrderedMessageStream
 
   def this() = this(ActorSystem("QueueReorderSpec"))
 
@@ -28,24 +36,39 @@ class QueueReorderSpec(_system: ActorSystem)
     shutdown(system)
   }
 
-  "SourceQueue" should "send out a message when it polls" in {
-    val testProbe = TestProbe()
-    val sourceQueue = 
-      system.actorOf(SourceQueue.props(testProbe.ref, orderedMessageStream))
-    sourceQueue ! Poll
-    testProbe.expectMsg(500 millis, Message(0,0))
-  }
-
-
   "SourceQueue" should "remove head element from the stream when polled" in {
     val sourceQueue = TestActorRef[SourceQueue](
-      SourceQueue.props(TestProbe().ref, orderedMessageStream))
+      SourceQueue.props(TestProbe().ref, infiniteOrderedMessageStream))
     val startStream = sourceQueue.underlyingActor.state
 
     sourceQueue ! Poll
 
     val endStream = sourceQueue.underlyingActor.state
     endStream.head should equal(startStream.drop(1).head)
+  }
+
+  "SourceQueue" should "send out a message when it polls" in {
+    val testProbe = TestProbe()
+    val sourceQueue = 
+      system.actorOf(SourceQueue.props(testProbe.ref, infiniteOrderedMessageStream))
+    sourceQueue ! Poll
+    testProbe.expectMsg(500 millis, Message(0,0))
+  }
+
+
+  "SourceQueue" should "empty stream when poll called enough times" in {
+    val testProbe = TestProbe()
+    val messageStream = Stream.range(0, 10, 1).map(i => Message(i,i) )
+    val sourceQueue = 
+      system.actorOf(SourceQueue.props(testProbe.ref, messageStream))
+
+    messageStream.foreach(m => {
+      sourceQueue ! Poll
+      testProbe.expectMsg(500 millis, m)
+    })
+
+    sourceQueue ! Poll
+    testProbe.expectNoMsg(500 millis)
   }
 }
 
