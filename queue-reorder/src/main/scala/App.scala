804e1d01b6aa5c6bb98f720f98037e68233c6510
markc <markc@pageuppeople.com>
2018-04-20 08:55:25 +1000

    Refactor actors into own files

--- queue-reorder/src/main/scala/App.scala
+++ queue-reorder/src/main/scala/App.scala
@@ -1,6 +1,6 @@
 package com.hombredequeso.queueReorder
 
-import akka.actor.{ Actor, ActorLogging, ActorRef, ActorSystem, Props }
+import akka.actor.{ Actor, ActorLogging, ActorRef, ActorSystem, Props ,PoisonPill }
 import scala.concurrent.duration._
 
 trait BaseMessage[TKey] {
@@ -10,42 +10,9 @@ trait BaseMessage[TKey] {
 }
 
 case class Message(sequenceNumber: Long, entitySequenceNumber: Long, key: Long) extends BaseMessage[Long] {
-  // val key = 123
 }
 
-object SourceQueue {
-  def props(pipe: ActorRef, messageStream: Stream[Message]) = Props(new SourceQueue(pipe, messageStream))
-
-  final case class Poll()
-}
-
-class SourceQueue(pipe: ActorRef, var messageStream: Stream[Message]) extends Actor {
-  import SourceQueue._
-
-  def state = messageStream
-
-  def receive = {
-    case Poll =>{
-      if (!messageStream.isEmpty) {
-        pipe ! messageStream.head
-        messageStream = messageStream.tail
-      }
-    }
-  }
-}
-
-object MessageLogger {
-  def props() = Props(new MessageLogger())
-}
-
-class MessageLogger extends Actor with ActorLogging {
-  import MessageLogger._
-
-  def receive = {
-    case Message(x, y,id) =>
-      log.info(s"Message received (Message ${x}, ${y}, ${id})")
-  }
-}
+case class ThrowException(message: String){}
 
 object QueueReorder extends App {
   import SourceQueue._
@@ -54,23 +21,29 @@ object QueueReorder extends App {
   
   val system: ActorSystem = ActorSystem("helloAkka")
 
-  // val messageConsumer: ActorRef = system.actorOf(MessageLogger.props)
   val messageConsumers: Vector[ActorRef] = (0 to 2).map(i => system.actorOf(MessageLogger.props, name=s"logger${i}")).toVector
   val messageReorderer = system.actorOf(MessageReorderer.props[Long](
                                           0L, 
                                           l => l.toInt,
-                                          messageConsumers))
-                                          // Vector(messageConsumer)))
+                                          messageConsumers), name="messageReorderer")
 
+  // Run 5 messages in reverse
   // val messageStream = Stream.from(0).take(5).map(i => Message(i,i,123)).reverse
   val messageStream = Stream.from(0).map(i => Message(i,i,i))
 
-  val sourceQueue: ActorRef = system.actorOf(SourceQueue.props(messageReorderer, messageStream))
+  val sourceQueue: ActorRef = system.actorOf(SourceQueue.props(messageReorderer, messageStream ), name = "source-queue")
   
   val cancellable =   system.scheduler.schedule(
     2000 milliseconds,
     1000 milliseconds,
     sourceQueue,
     Poll
+    )
+
+  val cancellable2 =   system.scheduler.schedule(
+    2000 milliseconds,
+    2000 milliseconds,
+    messageConsumers(1),
+    ThrowException("testing throw")
     )
 }
